546201d96739342d4b0f13e7adfaa0ae646e8407
LCLP <lukas@lclpnet.work>
2023-03-20 00:53:10 +0100

    register commands deferred before server is starting

--- kibu-command-api/src/main/java/work/lclpnet/kibu/cmd/util/MinecraftCommandRegister.java
+++ kibu-command-api/src/main/java/work/lclpnet/kibu/cmd/util/MinecraftCommandRegister.java
@@ -10,17 +10,36 @@ import net.minecraft.server.command.ServerCommandSource;
 import work.lclpnet.kibu.cmd.type.CommandRegister;
 import work.lclpnet.kibu.cmd.type.Initializable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinecraftCommandRegister implements CommandRegister<ServerCommandSource>, Initializable {
 
-    private MinecraftServer server;
+    private final List<LiteralArgumentBuilder<ServerCommandSource>> deferred = new ArrayList<>();
+    private final Object mutex = new Object();
+    private MinecraftServer server = null;
 
     @Override
     public void init() {
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> this.server = server);
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            synchronized (mutex) {
+                this.server = server;
+                loadDeferred();
+            }
+        });
     }
 
     @Override
     public LiteralCommandNode<ServerCommandSource> register(LiteralArgumentBuilder<ServerCommandSource> command) {
+        if (command == null) throw new NullPointerException("Command cannot be null");
+
+        synchronized (mutex) {
+            if (server == null) {
+                // server is not loaded yet, defer registration
+                deferred.add(command);
+            }
+        }
+
         var registered = CommandDispatcherUtils.register(getDispatcher(), command);
 
         syncCommandTree();
@@ -30,12 +49,20 @@ public class MinecraftCommandRegister implements CommandRegister<ServerCommandSo
 
     @Override
     public void unregister(LiteralCommandNode<ServerCommandSource> command) {
+        if (server == null) return;  // cannot register yet
+
         CommandDispatcherUtils.unregister(getDispatcher(), command);
 
         syncCommandTree();
     }
 
+    private void loadDeferred() {
+        this.deferred.forEach(this::register);
+        this.deferred.clear();
+    }
+
     private CommandDispatcher<ServerCommandSource> getDispatcher() {
+        if (this.server == null) throw new IllegalStateException("Server is not initialized yet");
         return this.server.getCommandManager().getDispatcher();
     }
 

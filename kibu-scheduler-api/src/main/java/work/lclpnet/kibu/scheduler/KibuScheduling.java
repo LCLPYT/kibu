cfec8fbaee28105111e2ba522897d2e268e65c33
LCLP <lukas@lclpnet.work>
2023-03-19 01:24:43 +0100

    introduce root scheduler

--- kibu-scheduler-api/src/main/java/work/lclpnet/kibu/scheduler/KibuScheduling.java
+++ kibu-scheduler-api/src/main/java/work/lclpnet/kibu/scheduler/KibuScheduling.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import work.lclpnet.kibu.scheduler.api.Scheduler;
 
 import javax.annotation.Nonnull;
 
@@ -12,16 +11,16 @@ public class KibuScheduling implements ModInitializer {
 
     public static final String MOD_ID = "kibu-scheduler-api";
     private static final Logger logger = LoggerFactory.getLogger(MOD_ID);
-    private static final Scheduler globalScheduler = new Scheduler(logger);
+    private static final RootScheduler rootScheduler = new RootScheduler(logger);
 
     @Override
     public void onInitialize() {
-        ServerTickEvents.START_SERVER_TICK.register(server -> globalScheduler.tick());
+        ServerTickEvents.START_SERVER_TICK.register(server -> rootScheduler.tick());
     }
 
     @SuppressWarnings("unused")
     @Nonnull
-    public static Scheduler getScheduler() {
-        return globalScheduler;
+    public static RootScheduler getRootScheduler() {
+        return rootScheduler;
     }
 }
---
+++ kibu-scheduler-api/src/main/java/work/lclpnet/kibu/scheduler/RootScheduler.java
@@ -0,0 +1,88 @@
+package work.lclpnet.kibu.scheduler;
+
+import org.slf4j.Logger;
+import work.lclpnet.kibu.scheduler.api.Scheduler;
+
+public class RootScheduler extends Scheduler {
+
+    private final Object mutex = new Object();
+    private Scheduler[] children;
+    private final ThreadLocal<Boolean> deadlock = ThreadLocal.withInitial(() -> false);
+
+    public RootScheduler(Logger logger) {
+        super(logger);
+        this.children = new RootScheduler[0];
+    }
+
+    /**
+     * Add a child to this scheduler. The parent will invoke <code>child.tick()</code> on tick.
+     * Children may not modify their parent's children from their <code>tick()</code> method.
+     * Use {@link RootScheduler#immediate(Runnable)} of the parent instead.
+     *
+     * @param child The child to add.
+     */
+    public void addChild(RootScheduler child) {
+        if (child == null) throw new NullPointerException("Child must not be null");
+
+        if (Boolean.TRUE.equals(deadlock.get())) {
+            throw new IllegalStateException("Modification of root scheduler during iteration (deadlock)");
+        }
+
+        synchronized (mutex) {
+            var newChildren = new Scheduler[this.children.length + 1];
+            System.arraycopy(this.children, 0, newChildren, 0, this.children.length);
+            newChildren[this.children.length] = child;
+
+            this.children = newChildren;
+        }
+    }
+
+    /**
+     * Remove a child from this scheduler.
+     * Children may not modify their parent's children from their <code>tick()</code> method.
+     * Use {@link RootScheduler#immediate(Runnable)} of the parent instead.
+     *
+     * @param child The child to remove.
+     */
+    public void removeChild(RootScheduler child) {
+        if (child == null) return;
+
+        if (Boolean.TRUE.equals(deadlock.get())) {
+            throw new IllegalStateException("Modification of root scheduler during iteration (deadlock)");
+        }
+
+        synchronized (mutex) {
+            int idx = -1;
+
+            for (int i = 0; i < this.children.length; i++) {
+                if (child.equals(this.children[i])) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx == -1) return;
+
+            var newChildren = new Scheduler[this.children.length - 1];
+            System.arraycopy(this.children, 0, newChildren, 0, idx);
+            System.arraycopy(this.children, idx + 1, newChildren, idx, this.children.length - idx - 1);
+
+            this.children = newChildren;
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        synchronized (mutex) {
+            deadlock.set(true);
+
+            for (var child : children) {
+                child.tick();
+            }
+
+            deadlock.set(false);
+        }
+    }
+}

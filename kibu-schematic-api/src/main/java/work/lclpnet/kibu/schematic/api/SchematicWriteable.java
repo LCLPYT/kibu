9787f2cda44d42b7fa0f5838ee47500eb540812b
LCLP <lukas@lclpnet.work>
2023-04-01 03:05:06 +0200

    fix null pointer exception with empty structures

--- kibu-schematic-api/src/main/java/work/lclpnet/kibu/schematic/api/SchematicWriteable.java
+++ kibu-schematic-api/src/main/java/work/lclpnet/kibu/schematic/api/SchematicWriteable.java
@@ -4,10 +4,15 @@ import work.lclpnet.kibu.mc.BlockEntity;
 import work.lclpnet.kibu.mc.BlockPos;
 import work.lclpnet.kibu.mc.BlockState;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public interface SchematicWriteable extends Cuboid {
 
+    @Nonnull
     BlockState getBlockState(BlockPos pos);
 
+    @Nullable
     BlockEntity getBlockEntity(BlockPos pos);
 
     int getDataVersion();
--- kibu-schematic-api/src/test/java/work/lclpnet/kibu/schematic/sponge/WriterV2Test.java
+++ kibu-schematic-api/src/test/java/work/lclpnet/kibu/schematic/sponge/WriterV2Test.java
@@ -24,4 +24,23 @@ class WriterV2Test {
         var schematic = nbt.getCompound("Schematic");
         assertEquals(10, schematic.keySet().size());
     }
+
+    @Test
+    public void testWriteWithAir() {
+        var structure = new SimpleBlockStructure(123);
+        structure.setBlockState(new BlockPos(0, 0, 0), new TestBlockState("minecraft:stone"));
+        structure.setBlockState(new BlockPos(0, 1, 0), new TestBlockState("minecraft:dirt"));
+        structure.setBlockState(new BlockPos(1, 1, 0), new TestBlockState("minecraft:grass_block"));
+        // bounds are now [0,0,0] to [1, 1, 0]. block at [1, 0, 0] is air
+
+        var writer = new WriterV2();
+        var nbt = writer.write(structure);
+
+        assertNotNull(nbt);
+        assertEquals(1, nbt.keySet().size());
+
+        var schematic = nbt.getCompound("Schematic");
+        assertEquals(10, schematic.keySet().size());
+        assertEquals(4, schematic.getCompound("Palette").keySet().size());
+    }
 }

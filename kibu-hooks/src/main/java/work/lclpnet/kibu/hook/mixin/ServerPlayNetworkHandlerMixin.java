75cc931158c148a0ffb0afcd10417742d7f8fb7b
LCLP <lukas@lclpnet.work>
2023-04-15 00:37:16 +0200

    add player slot change hook

--- kibu-hooks/src/main/java/work/lclpnet/kibu/hook/mixin/ServerPlayNetworkHandlerMixin.java
+++ kibu-hooks/src/main/java/work/lclpnet/kibu/hook/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,5 +1,6 @@
 package work.lclpnet.kibu.hook.mixin;
 
+import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -7,8 +8,11 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import work.lclpnet.kibu.hook.player.PlayerHooks;
+import work.lclpnet.kibu.hook.player.PlayerSlotChangeHook;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
@@ -28,5 +32,13 @@ public class ServerPlayNetworkHandlerMixin {
         if (text != null) {
             instance.broadcast(text, overlay);
         }
+    }
+
+    @Inject(
+            method = "onUpdateSelectedSlot",
+            at = @At("TAIL")
+    )
+    public void illwalls$onUpdateSelectedSlot(UpdateSelectedSlotC2SPacket packet, CallbackInfo ci) {
+        PlayerSlotChangeHook.HOOK.invoker().onChangeSlot(packet.getSelectedSlot());
     }
 }

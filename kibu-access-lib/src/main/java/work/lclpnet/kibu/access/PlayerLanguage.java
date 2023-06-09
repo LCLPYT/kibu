e432de0fa6ff6826b18ff2888dba6d008ed15ae0
LCLP <lukas@lclpnet.work>
2023-06-09 18:43:19 +0200

    add PlayerLanguage::getLanguage to detect a client's language

---
+++ kibu-access-lib/src/main/java/work/lclpnet/kibu/access/PlayerLanguage.java
@@ -0,0 +1,23 @@
+package work.lclpnet.kibu.access;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import work.lclpnet.kibu.access.type.LanguageGetter;
+
+import javax.annotation.Nonnull;
+
+public class PlayerLanguage {
+
+    private PlayerLanguage() {}
+
+    /**
+     * Gets the currently selected language of a player.
+     * @param player The player.
+     * @return The language code, such as "en_us".
+     * @implNote In case this method is invoked before a client sends the
+     * {@link net.minecraft.network.packet.c2s.play.ClientSettingsC2SPacket}, the language defaults to "en_us".
+     */
+    @Nonnull
+    public static String getLanguage(ServerPlayerEntity player) {
+        return ((LanguageGetter) player).kibu$getLanguage();
+    }
+}

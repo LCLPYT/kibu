18fb873d195c8352ec1770a93e04cc8ad8c10451
LCLP <lukas@lclpnet.work>
2023-06-21 17:22:29 +0200

    add ArmorStandAccess

---
+++ kibu-access-lib/src/main/java/work/lclpnet/kibu/access/entity/ArmorStandAccess.java
@@ -0,0 +1,15 @@
+package work.lclpnet.kibu.access.entity;
+
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import work.lclpnet.kibu.access.mixin.ArmorStandEntityAccessor;
+
+public class ArmorStandAccess {
+
+    public static void setSmall(ArmorStandEntity entity, boolean small) {
+        ((ArmorStandEntityAccessor) entity).invokeSetSmall(small);
+    }
+
+    public static void setMarker(ArmorStandEntity entity, boolean marker) {
+        ((ArmorStandEntityAccessor) entity).invokeSetMarker(marker);
+    }
+}

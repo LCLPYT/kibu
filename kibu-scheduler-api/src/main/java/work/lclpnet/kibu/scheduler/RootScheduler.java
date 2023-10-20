03da2f93176b61becd1219907bd188a398553a85
LCLP <lukas@lclpnet.work>
2023-10-20 02:43:54 +0200

    support RootScheduler co-modification (executed next tick)

--- kibu-scheduler-api/src/main/java/work/lclpnet/kibu/scheduler/RootScheduler.java
+++ kibu-scheduler-api/src/main/java/work/lclpnet/kibu/scheduler/RootScheduler.java
@@ -3,10 +3,15 @@ package work.lclpnet.kibu.scheduler;
 import org.slf4j.Logger;
 import work.lclpnet.kibu.scheduler.api.Scheduler;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class RootScheduler extends Scheduler {
 
-    private final Object mutex = new Object();
+    private final Object mutex = new Object(), offMutex = new Object();
     private Scheduler[] children;
+    private volatile List<Scheduler> addQueue = null, removeQueue = null;
     private final ThreadLocal<Boolean> deadlock = ThreadLocal.withInitial(() -> false);
 
     public RootScheduler(Logger logger) {
@@ -16,8 +21,6 @@ public class RootScheduler extends Scheduler {
 
     /**
      * Add a child to this scheduler. The parent will invoke <code>child.tick()</code> on tick.
-     * Children may not modify their parent's children from their <code>tick()</code> method.
-     * Use {@link RootScheduler#immediate(Runnable)} of the parent instead.
      *
      * @param child The child to add.
      */
@@ -25,7 +28,15 @@ public class RootScheduler extends Scheduler {
         if (child == null) throw new NullPointerException("Child must not be null");
 
         if (Boolean.TRUE.equals(deadlock.get())) {
-            throw new IllegalStateException("Modification of root scheduler during iteration (deadlock)");
+            synchronized (offMutex) {
+                if (addQueue == null) {
+                    addQueue = new ArrayList<>();
+                }
+
+                addQueue.add(child);
+            }
+
+            return;
         }
 
         synchronized (mutex) {
@@ -37,10 +48,37 @@ public class RootScheduler extends Scheduler {
         }
     }
 
+    public void addChildren(Collection<? extends Scheduler> children) {
+        if (children.isEmpty()) return;
+
+        if (Boolean.TRUE.equals(deadlock.get())) {
+            synchronized (offMutex) {
+                if (addQueue == null) {
+                    addQueue = new ArrayList<>();
+                }
+
+                addQueue.addAll(children);
+            }
+
+            return;
+        }
+
+        synchronized (mutex) {
+            var newChildren = new Scheduler[this.children.length + children.size()];
+            System.arraycopy(this.children, 0, newChildren, 0, this.children.length);
+
+            int i = this.children.length;
+
+            for (Scheduler scheduler : children) {
+                newChildren[i++] = scheduler;
+            }
+
+            this.children = newChildren;
+        }
+    }
+
     /**
      * Remove a child from this scheduler.
-     * Children may not modify their parent's children from their <code>tick()</code> method.
-     * Use {@link RootScheduler#immediate(Runnable)} of the parent instead.
      *
      * @param child The child to remove.
      */
@@ -48,7 +86,15 @@ public class RootScheduler extends Scheduler {
         if (child == null) return;
 
         if (deadlock.get()) {
-            throw new IllegalStateException("Modification of root scheduler during iteration (deadlock)");
+            synchronized (offMutex) {
+                if (removeQueue == null) {
+                    removeQueue = new ArrayList<>();
+                }
+
+                removeQueue.add(child);
+            }
+
+            return;
         }
 
         synchronized (mutex) {
@@ -71,9 +117,72 @@ public class RootScheduler extends Scheduler {
         }
     }
 
+    public void removeChildren(Collection<? extends Scheduler> children) {
+        if (children.isEmpty()) return;
+
+        if (deadlock.get()) {
+            synchronized (offMutex) {
+                if (removeQueue == null) {
+                    removeQueue = new ArrayList<>();
+                }
+
+                removeQueue.addAll(children);
+            }
+
+            return;
+        }
+
+        synchronized (mutex) {
+            int expected = children.size();
+            int[] indices = new int[expected];
+            int count = 0;
+
+            for (int i = 0; i < this.children.length && count < expected; i++) {
+                if (children.contains(this.children[i])) {
+                    indices[count++] = i;
+                }
+            }
+
+            if (count == 0) return;
+
+            var newChildren = new Scheduler[this.children.length - count];
+
+            int cursor = 0, newCursor = 0;
+
+            for (int i = 0; i < count; i++) {
+                int idx = indices[i];
+
+                int len = idx - cursor;
+
+                System.arraycopy(this.children, cursor, newChildren, newCursor, len);
+
+                cursor = idx + 1;
+                newCursor += len;
+            }
+
+            if (newCursor < newChildren.length) {
+                System.arraycopy(this.children, cursor, newChildren, newCursor, newChildren.length - newCursor);
+            }
+
+            this.children = newChildren;
+        }
+    }
+
     @Override
     public void tick() {
         super.tick();
+
+        synchronized (offMutex) {
+            if (addQueue != null) {
+                addChildren(addQueue);
+                addQueue.clear();
+            }
+
+            if (removeQueue != null) {
+                removeChildren(removeQueue);
+                removeQueue.clear();
+            }
+        }
 
         synchronized (mutex) {
             deadlock.set(true);
--- kibu-scheduler-api/src/test/java/work/lclpnet/kibu/scheduler/api/RootSchedulerTest.java
+++ kibu-scheduler-api/src/test/java/work/lclpnet/kibu/scheduler/api/RootSchedulerTest.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import work.lclpnet.kibu.scheduler.RootScheduler;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RootSchedulerTest {
 
@@ -28,6 +31,26 @@ public class RootSchedulerTest {
     }
 
     @Test
+    public void addChildren() {
+        var root = new RootScheduler(logger);
+        var child1 = new Scheduler(logger);
+        var child2 = new Scheduler(logger);
+
+        root.addChildren(List.of(child1, child2));
+
+        var flag1 = new AtomicBoolean(false);
+        var flag2 = new AtomicBoolean(false);
+
+        child1.immediate(() -> flag1.set(true));
+        child2.immediate(() -> flag2.set(true));
+
+        root.tick();
+
+        assertTrue(flag1.get());
+        assertTrue(flag2.get());
+    }
+
+    @Test
     public void removeChild() {
         var root = new RootScheduler(logger);
         var child = new Scheduler(logger);
@@ -43,13 +66,146 @@ public class RootSchedulerTest {
     }
 
     @Test
-    public void testDeadlock() {
-        final var root = new RootScheduler(logger);
-        final var child = new Scheduler(logger);
-        root.addChild(child);
+    public void removeChildren_all() {
+        var root = new RootScheduler(logger);
+        var child1 = new Scheduler(logger);
+        var child2 = new Scheduler(logger);
+        root.addChild(child1);
+        root.addChild(child2);
 
-        child.immediate(() -> assertThrows(IllegalStateException.class, () -> root.removeChild(child)));
+        var flag1 = new AtomicBoolean(false);
+        var flag2 = new AtomicBoolean(false);
+
+        child1.immediate(() -> flag1.set(true));
+        child2.immediate(() -> flag2.set(true));
+
+        root.removeChildren(Set.of(child1, child2));
+        root.tick();
+
+        assertFalse(flag1.get());
+        assertFalse(flag2.get());
+    }
+
+    @Test
+    public void removeChildren_trailing() {
+        var root = new RootScheduler(logger);
+        Scheduler child1 = new Scheduler(logger), child2 = new Scheduler(logger), child3 = new Scheduler(logger);
+        root.addChildren(List.of(child1, child2, child3));
+
+        AtomicBoolean flag1 = new AtomicBoolean(false),
+                flag2 = new AtomicBoolean(false),
+                flag3 = new AtomicBoolean(false);
+
+        child1.immediate(() -> flag1.set(true));
+        child2.immediate(() -> flag2.set(true));
+        child3.immediate(() -> flag3.set(true));
+
+        root.removeChildren(Set.of(child1, child2));
+        root.tick();
+
+        assertFalse(flag1.get());
+        assertFalse(flag2.get());
+        assertTrue(flag3.get());
+    }
+
+    @Test
+    public void removeChildren_middle() {
+        var root = new RootScheduler(logger);
+        Scheduler child1 = new Scheduler(logger), child2 = new Scheduler(logger), child3 = new Scheduler(logger);
+        root.addChildren(List.of(child1, child2, child3));
+
+        AtomicBoolean flag1 = new AtomicBoolean(false),
+                flag2 = new AtomicBoolean(false),
+                flag3 = new AtomicBoolean(false);
+
+        child1.immediate(() -> flag1.set(true));
+        child2.immediate(() -> flag2.set(true));
+        child3.immediate(() -> flag3.set(true));
+
+        root.removeChildren(Set.of(child1, child3));
+        root.tick();
+
+        assertFalse(flag1.get());
+        assertTrue(flag2.get());
+        assertFalse(flag3.get());
+    }
+
+    @Test
+    public void removeChildren_preTrailing() {
+        var root = new RootScheduler(logger);
+        Scheduler child1 = new Scheduler(logger), child2 = new Scheduler(logger), child3 = new Scheduler(logger);
+        root.addChildren(List.of(child1, child2, child3));
+
+        AtomicBoolean flag1 = new AtomicBoolean(false),
+                flag2 = new AtomicBoolean(false),
+                flag3 = new AtomicBoolean(false);
+
+        child1.immediate(() -> flag1.set(true));
+        child2.immediate(() -> flag2.set(true));
+        child3.immediate(() -> flag3.set(true));
+
+        root.removeChildren(Set.of(child2, child3));
+        root.tick();
+
+        assertTrue(flag1.get());
+        assertFalse(flag2.get());
+        assertFalse(flag3.get());
+    }
+
+    @Test
+    public void addChild_testCoModification() {
+        final var root = new RootScheduler(logger);
+        final var child1 = new Scheduler(logger);
+        final var child2 = new Scheduler(logger);
+        root.addChild(child1);
+
+        AtomicBoolean flag1 = new AtomicBoolean();
+        AtomicBoolean flag2 = new AtomicBoolean();
+
+        child1.immediate(() -> {
+            flag1.set(true);
+            root.addChild(child2);
+        });
+
+        child2.immediate(() -> flag2.set(true));
 
         root.tick();
+
+        assertTrue(flag1.get());
+        assertFalse(flag2.get());
+
+        root.tick();
+
+        assertTrue(flag2.get());
+    }
+
+    @Test
+    public void removeChild_testCoModification() {
+        final var root = new RootScheduler(logger);
+        final var child1 = new Scheduler(logger);
+        final var child2 = new Scheduler(logger);
+        root.addChild(child1);
+        root.addChild(child2);
+
+        AtomicBoolean flag1 = new AtomicBoolean();
+        AtomicBoolean flag2 = new AtomicBoolean();
+
+        child1.immediate(() -> {
+            flag1.set(true);
+            root.removeChild(child2);
+        });
+
+        child2.immediate(() -> flag2.set(true));
+
+        root.tick();
+
+        assertTrue(flag1.get());
+        assertTrue(flag2.get());
+
+        flag2.set(false);
+
+        root.tick();
+
+        assertFalse(flag2.get());
     }
 }

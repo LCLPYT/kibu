9614dfcb1d5a471ffa6531c858cf02bf78e8ccef
LCLP <lukas@lclpnet.work>
2023-12-17 19:06:53 +0100

    fire armor stand damage event too

--- kibu-hooks/src/main/java/work/lclpnet/kibu/hook/mixin/ArmorStandEntityMixin.java
+++ kibu-hooks/src/main/java/work/lclpnet/kibu/hook/mixin/ArmorStandEntityMixin.java
@@ -1,6 +1,7 @@
 package work.lclpnet.kibu.hook.mixin;
 
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import work.lclpnet.kibu.hook.entity.ArmorStandManipulateCallback;
+import work.lclpnet.kibu.hook.entity.NonLivingDamageCallback;
 
 @Mixin(ArmorStandEntity.class)
 public class ArmorStandEntityMixin {
@@ -23,6 +25,19 @@ public class ArmorStandEntityMixin {
         ArmorStandEntity self = (ArmorStandEntity) (Object) this;
 
         if (ArmorStandManipulateCallback.HOOK.invoker().onManipulate(self, player, slot, stack, hand)) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(
+            method = "damage",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void kibu$beforeDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        ArmorStandEntity self = (ArmorStandEntity) (Object) this;
+
+        if (NonLivingDamageCallback.HOOK.invoker().onDamage(self, source, amount)) {
             cir.setReturnValue(false);
         }
     }

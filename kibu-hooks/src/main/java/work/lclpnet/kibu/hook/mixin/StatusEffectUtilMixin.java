f9206126dc74fcfe155e3cf822fff9e1c6130719
LCLP <lukas@lclpnet.work>
2024-04-06 18:04:23 +0200

    prevent elder guardian effect if status effect was cancelled

---
+++ kibu-hooks/src/main/java/work/lclpnet/kibu/hook/mixin/StatusEffectUtilMixin.java
@@ -0,0 +1,34 @@
+package work.lclpnet.kibu.hook.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import work.lclpnet.kibu.hook.entity.EntityStatusEffectCallback;
+
+@Mixin(StatusEffectUtil.class)
+public class StatusEffectUtilMixin {
+
+    @Inject(
+            method = "method_42145",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private static void kibu$onTargetPlayerForStatusEffect(
+            Entity entity, Vec3d vec3d, double d, StatusEffect statusEffect, StatusEffectInstance statusEffectInstance,
+            int i, ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+
+        if (!cir.getReturnValueZ()) return;
+
+        // player would be targeted
+        if (EntityStatusEffectCallback.HOOK.invoker().onAddEffect(player, statusEffectInstance, entity)) {
+            cir.setReturnValue(false);
+        }
+    }
+}

9877945c335c1c0669964f51d06552e01912c7fe
LCLP <lukas@lclpnet.work>
2024-05-10 17:18:50 +0200

    suppress empty ItemStack old nbt format error

---
+++ kibu-schematic-fabric/src/main/java/work/lclpnet/kibu/schematic/mixin/ItemStackMixin.java
@@ -0,0 +1,28 @@
+package work.lclpnet.kibu.schematic.mixin;
+
+import net.minecraft.item.ItemStack;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+
+    @Shadow @Final private static Logger LOGGER;
+
+    @Inject(
+            method = "method_57377",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void kibu$suppressEmptyItemError(String error, CallbackInfo ci) {
+        // this error is thrown when trying to load nbt with the old item format (e.g. from schematics)
+        if (error.equals("Item must not be minecraft:air")) {
+            ci.cancel();
+        }
+    }
+}

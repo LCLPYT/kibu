10bc4142c7534df90cea9e67c7c67c44608e42d1
LCLP <lukas@lclpnet.work>
2025-03-27 20:34:21 +0100

    add FarmlandMoistureChangeCallback

---
+++ kibu-hooks/src/main/java/work/lclpnet/kibu/hook/world/FarmlandMoistureChangeCallback.java
@@ -0,0 +1,33 @@
+package work.lclpnet.kibu.hook.world;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import work.lclpnet.kibu.hook.Hook;
+import work.lclpnet.kibu.hook.HookFactory;
+
+/**
+ * Called when the moisture of a farmland block changes.
+ */
+public interface FarmlandMoistureChangeCallback {
+
+    Hook<FarmlandMoistureChangeCallback> HOOK = HookFactory.createArrayBacked(FarmlandMoistureChangeCallback.class, hooks -> (world, pos, moisture) -> {
+        boolean cancel = false;
+
+        for (var hook : hooks) {
+            if (hook.onMoistureChange(world, pos, moisture)) {
+                cancel = true;
+            }
+        }
+
+        return cancel;
+    });
+
+    /**
+     * Called when the moisture of a farmland block changes.
+     * @param world The world.
+     * @param pos The block position of the farmland
+     * @param moisture The moisture level after the change is complete. Will be -1 when the farmland would be converted to dirt.
+     * @return True, if the change should be cancelled. False for default behaviour.
+     */
+    boolean onMoistureChange(ServerWorld world, BlockPos pos, int moisture);
+}
